5f51ea7b98188ce980393f52e83d575550742f6d
Bharat99978 <166504281+Bharat99978@users.noreply.github.com>
2024-10-31 14:05:14 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,22 +5,22 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '7228147192:AAEg1GtZGTGSr_uag1BMi2V6hwytNBBYb8o';
 const bot = new TelegramBot(token, { polling: true });
 
-// Message to be sent every 30 minutes
-const message = "Hello! This is your scheduled message every 30 minutes.";
+// Message to be sent every 15 minutes
+const message = "मैं झुकूंगा नहीं";
 
-// Start bot and send a message every 30 minutes
+// Start bot and send a message every 15 minutes
 bot.on('polling_error', console.log); // Error handling
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
   // Send an initial message when the bot starts
-  bot.sendMessage(chatId, "Bot started! You will receive messages every 30 minutes.");
+  bot.sendMessage(chatId, "Bot started! You will receive messages every 15 minutes.");
 
-  // Set an interval to send a message every 30 minutes (30 * 60 * 1000 milliseconds)
+  // Set an interval to send a message every 15 minutes (15 * 60 * 1000 milliseconds)
   setInterval(() => {
     bot.sendMessage(chatId, message);
-  }, 30 * 60 * 1000);
+  }, 15 * 60 * 1000);
 });
 
 console.log("Telegram bot is running...");
